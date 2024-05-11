4daecd7564696bd09e957935f2715ec964b5f21f
Muhammad Nabeel <nabeelmajeed266@gmail.com>
2024-05-11 20:26:28 +0500

    Initial Commit for App Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-3xl text-gray-800 font-montAlter font-bold">
+      React Real Estate Application
+    </h1>
+  );
+};
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        mobileSm: "320px",
+        mobileRg: "376px",
+        tabletSm: "480px",
+        tabletRg: "640px",
+        tabletLg: "768px",
+        laptopSm: "901px",
+        laptopRg: "1024px",
+        desktopSm: "1200px",
+        desktopRg: "1350px",
+        desktopLg: "1536px",
+      },
+    },
+    fontFamily: {
+      montAlter: ["'Montserrat Alternates'", "sans-serif"],
+      mont: ["'Montserrat'", "sans-serif"],
+      quick: ["'Quicksand'", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
