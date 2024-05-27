b18d75bc6d6789e9d2cf17c3f63b1af0634bb860
Muhammad Nabeel <nabeelmajeed266@gmail.com>
2024-05-27 08:29:50 +0500

    create a user Schema

---
+++ api/Models/user-schema.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.listen(8000, () => {
-  console.log(`Server is running on PORT number 8000`);
+// Connect Database and Run the Server
+(async function () {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("You successfully connected to MongoDB");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running successfully on PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error + " ==> Connection Problem");
+    process.exit(1);
+  }
+})();
+
+process.on("SIGINT", async function () {
+  console.log("App is terminating");
+  process.exit(0);
 });
