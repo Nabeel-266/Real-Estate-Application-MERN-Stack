c5265db66d42015262d8a06b24d370546789bac1
Muhammad Nabeel <nabeelmajeed266@gmail.com>
2024-06-03 11:10:48 +0500

    username proper to set and trim apply on username and email for stoping non conventional errors

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,6 +3,25 @@ import { SIGNUP } from "../constants/apisRoute";
 
 // For SIGNUP
 export const registerUser = async (formData) => {
-  const response = await axios.post(`${SIGNUP}`, formData);
+  const { username, email, password, confirmPassword } = formData;
+
+  const userFullname = username?.trim();
+
+  const firstname =
+    userFullname?.split(" ")[0].charAt(0).toLocaleUpperCase() +
+    userFullname?.split(" ")[0].slice(1).toLocaleLowerCase();
+
+  const lastname =
+    userFullname?.split(" ")[1].charAt(0).toLocaleUpperCase() +
+    userFullname?.split(" ")[1].slice(1).toLocaleLowerCase();
+
+  const userCredentials = {
+    username: `${firstname} ${lastname}`,
+    email: email.trim(),
+    password,
+    confirmPassword,
+  };
+
+  const response = await axios.post(`${SIGNUP}`, userCredentials);
   return response.data;
 };
