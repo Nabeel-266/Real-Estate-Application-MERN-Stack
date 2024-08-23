a473f5c164281e90e915f17ad305c522dceb27bf
Muhammad Nabeel <nabeelmajeed266@gmail.com>
2024-08-24 01:41:10 +0500

    Application Routing change with Create Browser Router method in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,8 @@
 import { useCallback, useEffect, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { checkToken } from "./api/authAPIs";
+import { ToastContainer } from "react-toastify";
 
 // Import Protected Route Components
 import {
@@ -15,7 +11,8 @@ import {
 } from "./secure/Protected_Route";
 
 // Import Pages & Components
-import { ToastContainer } from "react-toastify";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import About from "./pages/About";
@@ -23,32 +20,118 @@ import Contact from "./pages/Contact";
 import AddProperty from "./pages/AddProperty";
 import ProfileDemo from "./pages/ProfileDemo";
 import Auth from "./pages/Auth";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import Signin from "./components/Authentication/Signin";
 import Signup from "./components/Authentication/Signup";
+import Signin from "./components/Authentication/Signin";
 import VerifyAccount from "./components/Authentication/VerifyAccount";
+import ForgotPassword from "./components/Authentication/ForgotPassword";
 import User from "./pages/User";
 import Profile from "./components/User/Profile";
 import Account from "./components/User/Account";
 import MyProperties from "./components/User/MyProperties";
-import ForgotPassword from "./components/Authentication/ForgotPassword";
 import NotFound from "./pages/NotFound";
 
-const AppRoutes = () => {
-  const location = useLocation();
-  const dispatch = useDispatch();
+const AppLayout = () => {
   const [isOpenSidebar, setIsOpenSidebar] = useState(false);
 
-  // Check Header is shown or not?
-  const routeLocation = location.pathname;
-  const isHeaderShow =
-    routeLocation !== "/account/sign-in" &&
-    routeLocation !== "/account/sign-up" &&
-    routeLocation !== "/account/verification" &&
-    routeLocation !== "/account/forgot-password";
+  return (
+    <>
+      <Header setIsOpenSidebar={setIsOpenSidebar} />
+      <Sidebar
+        isOpenSidebar={isOpenSidebar}
+        setIsOpenSidebar={setIsOpenSidebar}
+      />
+      <Outlet />
+    </>
+  );
+};
 
-  // Check User Token Authorization
+// Routes Configuration
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/explore",
+        element: <Explore />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/add-property",
+        element: (
+          <UnAuthProtectedRoute>
+            <AddProperty />
+          </UnAuthProtectedRoute>
+        ),
+      },
+      {
+        element: (
+          <UnAuthProtectedRoute>
+            <User />
+          </UnAuthProtectedRoute>
+        ),
+        children: [
+          {
+            path: "/user/profile",
+            element: <Profile />,
+          },
+          {
+            path: "/user/account",
+            element: <Account />,
+          },
+          {
+            path: "/user/property",
+            element: <MyProperties />,
+          },
+        ],
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+  {
+    element: (
+      <AuthProtectedRoute>
+        <Auth />
+      </AuthProtectedRoute>
+    ),
+    children: [
+      {
+        path: "/account/sign-in",
+        element: <Signin />,
+      },
+      {
+        path: "/account/sign-up",
+        element: <Signup />,
+      },
+      {
+        path: "/account/verification",
+        element: <VerifyAccount />,
+      },
+      {
+        path: "/account/forgot-password",
+        element: <ForgotPassword />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  const dispatch = useDispatch();
+
   const memoizedCheckToken = useCallback(
     () => checkToken(dispatch),
     [dispatch]
@@ -60,74 +143,107 @@ const AppRoutes = () => {
 
   return (
     <div className="app bg-white relative overflow-hidden">
-      {isHeaderShow && (
-        <>
-          <Header setIsOpenSidebar={setIsOpenSidebar} />
-          <Sidebar
-            isOpenSidebar={isOpenSidebar}
-            setIsOpenSidebar={setIsOpenSidebar}
-          />
-        </>
-      )}
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route
-          element={
-            <AuthProtectedRoute>
-              <Auth />
-            </AuthProtectedRoute>
-          }
-        >
-          <Route path="/account/sign-in" element={<Signin />} />
-          <Route path="/account/sign-up" element={<Signup />} />
-          <Route path="/account/verification" element={<VerifyAccount />} />
-          <Route path="/account/forgot-password" element={<ForgotPassword />} />
-        </Route>
-
-        <Route
-          path="/user/"
-          element={
-            <UnAuthProtectedRoute>
-              <User />
-            </UnAuthProtectedRoute>
-          }
-        >
-          <Route path="profile" element={<Profile />} />
-          <Route path="account" element={<Account />} />
-          <Route path="property" element={<MyProperties />} />
-        </Route>
-
-        <Route path="/explore" element={<Explore />} />
-
-        <Route
-          path="/add-property"
-          element={
-            <UnAuthProtectedRoute>
-              <AddProperty />
-            </UnAuthProtectedRoute>
-          }
-        />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <RouterProvider router={router} />
+      <ToastContainer />
     </div>
   );
-};
-
-const App = () => {
-  return (
-    <>
-      <Router>
-        <AppRoutes />
-      </Router>
-
-      <ToastContainer />
-    </>
-  );
-};
+}
 
 export default App;
+
+// const AppRoutes = () => {
+//   const location = useLocation();
+//   const dispatch = useDispatch();
+//   const [isOpenSidebar, setIsOpenSidebar] = useState(false);
+
+//   // Check Header is shown or not?
+//   const routeLocation = location.pathname;
+//   const isHeaderShow =
+//     routeLocation !== "/account/sign-in" &&
+//     routeLocation !== "/account/sign-up" &&
+//     routeLocation !== "/account/verification" &&
+//     routeLocation !== "/account/forgot-password";
+
+//   // Check User Token Authorization
+//   const memoizedCheckToken = useCallback(
+//     () => checkToken(dispatch),
+//     [dispatch]
+//   );
+
+//   useEffect(() => {
+//     memoizedCheckToken();
+//   }, [memoizedCheckToken]);
+
+//   return (
+//     <div className="app bg-white relative overflow-hidden">
+//       {isHeaderShow && (
+//         <>
+//           <Header setIsOpenSidebar={setIsOpenSidebar} />
+//           <Sidebar
+//             isOpenSidebar={isOpenSidebar}
+//             setIsOpenSidebar={setIsOpenSidebar}
+//           />
+//         </>
+//       )}
+
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+
+//         <Route
+//           element={
+//             <AuthProtectedRoute>
+//               <Auth />
+//             </AuthProtectedRoute>
+//           }
+//         >
+//           <Route path="/account/sign-in" element={<Signin />} />
+//           <Route path="/account/sign-up" element={<Signup />} />
+//           <Route path="/account/verification" element={<VerifyAccount />} />
+//           <Route path="/account/forgot-password" element={<ForgotPassword />} />
+//         </Route>
+
+//         <Route
+//           path="/user/"
+//           element={
+//             <UnAuthProtectedRoute>
+//               <User />
+//             </UnAuthProtectedRoute>
+//           }
+//         >
+//           <Route path="profile" element={<Profile />} />
+//           <Route path="account" element={<Account />} />
+//           <Route path="property" element={<MyProperties />} />
+//         </Route>
+
+//         <Route path="/explore" element={<Explore />} />
+
+//         <Route
+//           path="/add-property"
+//           element={
+//             <UnAuthProtectedRoute>
+//               <AddProperty />
+//             </UnAuthProtectedRoute>
+//           }
+//         />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/contact" element={<Contact />} />
+
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <>
+//       <Router>
+//         <AppRoutes />
+//       </Router>
+
+//       <ToastContainer />
+//     </>
+//   );
+// };
+
+// export default App;
--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -22,14 +22,14 @@ const User = () => {
         <aside className="w-[20%] min-w-[25rem] h-full hidden laptopSm:flex laptopSm:items-center pl-[2%] desktopSm:pl-[4%]">
           <ul className="w-full h-[calc(100%-6rem)] flex flex-col gap-[0.3rem] border-r-[0.2rem] border-neutral-200">
             {[
-              [FiUser, "Profile", "profile"],
-              [RiAccountBoxLine, "Account", "account"],
-              [MdOutlineMapsHomeWork, "My Properties", "property"],
+              [FiUser, "Profile", "/user/profile"],
+              [RiAccountBoxLine, "Account", "/user/account"],
+              [MdOutlineMapsHomeWork, "My Properties", "/user/property"],
             ].map(([Icon, title, route], index) => (
               <li
                 key={index}
                 className={`${
-                  isActiveTab === route
+                  isActiveTab === route.split("/")[2]
                     ? "before:translate-x-[-5%] before:bg-theme-blue"
                     : "before:translate-x-[-100%] hover:before:translate-x-[0%] before:bg-neutral-100"
                 } w-[95%] relative overflow-hidden before:content-[''] before:w-full before:h-full before:z-0 before:absolute before:top-0 before:left-0 before:rounded-e-full before:transition before:ease-out before:duration-[0.4s]`}
@@ -37,7 +37,7 @@ const User = () => {
                 <Link
                   to={route}
                   className={`${
-                    isActiveTab === route
+                    isActiveTab === route.split("/")[2]
                       ? "text-white font-medium"
                       : "text-neutral-500 font-semibold"
                   } w-full relative z-10 flex items-center gap-[0.8rem] px-[1rem] py-[0.8rem]`}
