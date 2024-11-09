48102f0364ffbf82e612b682656fff96d5888928
Muhammad Nabeel <nabeelmajeed266@gmail.com>
2024-11-09 19:20:23 +0500

    create Top Cards Section in Listings Panel for showing status based Listing Stats

--- client/src/Admin/pages/Panels/Listing.jsx
+++ client/src/Admin/pages/Panels/Listing.jsx
@@ -1,7 +1,69 @@
 import React from "react";
 
+// Import React Icons
+import { FaHourglassHalf } from "react-icons/fa";
+import { BsBuildingFillCheck } from "react-icons/bs";
+import { MdAddHomeWork } from "react-icons/md";
+import { TbArrowBigRightLinesFilled } from "react-icons/tb";
+import { PiStarFourFill } from "react-icons/pi";
+
+// Example data for cards and table
+const listingsStats = [
+  {
+    title: "Finalized Properties",
+    count: 120,
+    icon: <BsBuildingFillCheck />,
+  },
+  {
+    title: "Published Properties",
+    count: 60,
+    icon: <MdAddHomeWork />,
+  },
+  {
+    title: "Pending Properties",
+    count: 15,
+    icon: <FaHourglassHalf />,
+  },
+];
+
 const Listing = () => {
-  return <div>Listing</div>;
+  return (
+    <div className="w-full flex flex-col gap-[3rem] px-[2rem] pt-[2rem] pb-[4rem]">
+      {/* Section Top */}
+      <section className="w-full flex justify-between gap-[2rem]">
+        {listingsStats.map((stat, index) => (
+          <div
+            key={index}
+            className="w-[32%] min-w-[26rem] max-w-[33rem] relative overflow-hidden bg-theme-blue rounded-xl shadow-[0.4rem_0.4rem_0.8rem_#00000060]"
+          >
+            <div
+              className={`absolute z-[1] top-0 bottom-0 right-[1.5rem] flex items-center justify-center text-[#ffffff30] ${
+                stat.title.includes("Total")
+                  ? "text-[7rem] mb-[0.3rem]"
+                  : stat.title.includes("Active")
+                  ? "text-[6.6rem]"
+                  : "text-[6rem]"
+              }`}
+            >
+              {stat.icon}
+            </div>
+
+            <div className="relative z-10 h-fit flex flex-col items-start gap-[1rem] text-white px-[1.5rem] pt-[1.2rem] pb-[1.2rem]">
+              <h2 className="text-[1.6rem] leading-[1.6rem] font-bold whitespace-nowrap">
+                {stat.title}
+              </h2>
+
+              <p className="flex items-center gap-[1rem] text-[3rem] leading-[2.8rem] font-bold">
+                <TbArrowBigRightLinesFilled size="2.2rem" />
+                <span className="text-theme-yellow ">{stat.count}</span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </section>
+      ;
+    </div>
+  );
 };
 
 export default Listing;
